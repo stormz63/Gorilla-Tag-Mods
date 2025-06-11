ef41bd41f622273f55874a703603bff0a5ee28c1
stormz63 <grady.korchinskI@gmail.com>
2025-06-10 20:48:45 -0600

    Add files via upload

---
+++ FunMods/FixGorilla.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GorillaTagMods.Mods.FunMods
+{
+    internal class FixGorilla
+    {
+        public static void FixGorillaMod()
+        {
+            var player = GorillaLocomotion.GTPlayer.Instance;
+            if (player != null)
+            {
+                player.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+    }
+}
